b2d1d58084e66f4ea27991894717ab8547a9767e
StevenBaezFernandez <stevenbaezfernandez@gmail.com>
2021-04-07 13:36:41 -0400

    basic functionality of navbar completed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  toggle_novia:boolean = false;
+  toggle_novio:boolean = false;
+  toggle_recepcion:boolean = false;
+  toggle_foto_video:boolean = false;
+  toggle_banquetes:boolean = false;
+  toggle_flores:boolean = false;
+
+  
+
   constructor() { }
 
   ngOnInit(): void {
